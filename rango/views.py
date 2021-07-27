8b0887f069dde227306adfe7f80a333772beaff8
JIhuaTian <2443894t@student.gla.ac.uk>
2021-07-27 19:59:01 +0800

    index hyperlink

--- rango/views.py
+++ rango/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Rango says hey there partner!\n<a href='/rango/about/'>About</a>")
 def about(request):
-    return HttpResponse("Rango says here is the about page.")
+    return HttpResponse("Rango says here is the about page.\n<a href='/rango/'>Index</a>")
