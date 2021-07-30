ee4f01a40b4ef769cd16107b5a20ba8d0dfa5f0b
JIhuaTian <2443894t@student.gla.ac.uk>
2021-07-30 21:13:10 +0800

    Site Counter Example

--- rango/views.py
+++ rango/views.py
@@ -11,6 +11,8 @@ from django.shortcuts import redirect
 from django.urls import reverse
 from rango.forms import UserForm, UserProfileForm
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
+
 
 def index(request):
     request.session.set_test_cookie()
@@ -19,6 +21,10 @@ def index(request):
     context_dict = {}
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
+    context_dict['visits'] = int(request.COOKIES.get('visits', '1'))
+    response = render(request, 'rango/index.html', context_dict)
+    visitor_cookie_handler(request, response)
+    return response
 
     return render(request,'rango/index.html',context=context_dict)
 def about(request):
@@ -132,3 +138,20 @@ def restricted(request):
 def user_logout(request):
     logout(request)
     return redirect(reverse('rango:index'))
+
+def visitor_cookie_handler(request, response):
+
+    visits = int(request.COOKIES.get('visits', '1'))
+    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+                                        '%Y-%m-%d %H:%M:%S')
+
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+
+        response.set_cookie('last_visit', str(datetime.now()))
+    else:
+
+        response.set_cookie('last_visit', last_visit_cookie)
+
+    response.set_cookie('visits', visits)
